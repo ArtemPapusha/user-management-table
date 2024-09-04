385afdd0033082ae7a90de98b94fa0539936b8ef
ArtemPapusha <art.papusha@gmail.com>
2024-09-04 16:47:49 +0900

    update gitingnore

--- src/components/SearchInput/styles.module.scss.d.ts
+++
@@ -1,2 +0,0 @@
-export declare const inputCloseButton: string
-export declare const inputWrapper: string
--- src/modules/UserManagementTable/styles.module.scss.d.ts
+++
@@ -1,3 +0,0 @@
-export declare const container: string
-export declare const spiner: string
-export declare const table: string
